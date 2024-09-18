16653aed92d1cd38db3d48d2e6198383b2c14687
LucasLira777 <lira.fire.2007@gmail.com>
2024-09-18 10:33:31 -0300

    metodo direção
    criação do metodo de direção

--- provaMatriz/Assets/scripts/Player.cs
+++ provaMatriz/Assets/scripts/Player.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    const float velocidade = 0;
+
+    [SerializeField]
+    bool jogador1;
+    [SerializeField]
+    Color corDoJogador;
+
+    private void direção()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
